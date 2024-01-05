a788a1c303c4b9ef45ef14e52bcf3809703f7a24
Kevin Vang <kevinvaj88@hotmail.com>
2024-01-04 21:05:42 -0800

    import not allow

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -3,3 +3,13 @@ import sys
 
 if __name__ == "__main__":
 
+    argc = len(argv) - 1
+
+    if argc == 0:
+        print("0 arguments.")
+    elif count == 1:
+        print("1 argument:")
+    else:
+        print("{} arguments:".format(argc))
+    for i in range(argc):
+        print("{}: {}".format(i + 1, argv[i + 1]))
