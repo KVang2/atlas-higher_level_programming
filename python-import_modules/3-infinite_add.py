896e5a61ee37e8011e6f282e7b671b8d23304abe
Kevin Vang <kevinvaj88@hotmail.com>
2024-01-04 20:50:41 -0800

    chmod

--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -4,7 +4,6 @@ import sys
 if __name__ == "__main__":
 
     sum = 0
-
-    for i in range(len(sys.argv)- 1):
+    for i in range(len(sys.argv) - 1):
         sum += int(sys.argv[i + 1])
     print("{}".format(sum))
