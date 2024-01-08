fb9828e9cc8e18e26a356a6d8598351728dbb5dd
Kevin Vang <kevinvaj88@hotmail.com>
2024-01-08 09:32:18 -0800

    test

--- python-data_structures/4-new_in_list.py
+++ python-data_structures/4-new_in_list.py
@@ -4,6 +4,6 @@ def new_in_list(my_list, idx, element):
         return my_list:
     elif idx >= len(my_list):
         return my_list:
-    list_2 = my_list:
+    list_2 = my_list
     list_2[idx] = element)
     return list_2
