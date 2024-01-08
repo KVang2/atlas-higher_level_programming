584762c2d138411f32f8ae901640414a6e6044e4
Kevin Vang <kevinvaj88@hotmail.com>
2024-01-08 15:53:22 -0800

    test

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    for i in range(len(maxtrix)):
-        for element in range(len(matrix[i])):
-            if element != len(matrix[row]) - 1:
-            print("{:d}".format(element), end=" ")
+    if not matrix:
         print()
+    else:
+        for row in matrix:
+            print(' '.join(map(str, row)))
