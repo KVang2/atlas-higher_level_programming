b1311b4efc253baff12105c73df5b265f88e40a8
Kevin Vang <kevinvaj88@hotmail.com>
2024-01-08 19:34:42 -0800

    test

--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -3,4 +3,4 @@ def multiple_returns(sentence):
     if not sentence:
         return (None, len(sentence))
     first_char = sentence[0]
-    return (first_char, len(sentence))
+    return (len(sentence), sentence[0])
