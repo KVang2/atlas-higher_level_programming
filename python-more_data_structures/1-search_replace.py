08795477d999157530afff218bb2d7786bae37f7
Kevin Vang <kevinvaj88@hotmail.com>
2024-01-09 15:33:58 -0800

    search

--- python-more_data_structures/1-search_replace.py
+++ python-more_data_structures/1-search_replace.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
     initial_list = list(my_list)
-    for i in range(len(n_list)):
+    for i in range(len(initial_list)):
         if initial_list[i] == search:
             initial_list[i] == replace
     return initial_list
