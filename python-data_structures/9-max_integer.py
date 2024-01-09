7273c632a13df4ed80146477bb072ef7220c2c29
Kevin Vang <kevinvaj88@hotmail.com>
2024-01-09 10:49:15 -0800

    max checking might work

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -2,10 +2,10 @@
 def max_integer(my_list=[]):
     if not my_list:
         return None
-    max_value = my_list[]
+    max_value = my_list[0]
 
-    for i in my_list[]:
+    for i in my_list:
         if i > max_value:
-            max_value = 1
+            max_value = i
 
     return max_value
