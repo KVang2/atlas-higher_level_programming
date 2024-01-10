4ca404859ec033afde3372d9652d2e1f7e9f0d2e
Kevin Vang <kevinvaj88@hotmail.com>
2024-01-09 19:29:40 -0800

    adding more

--- python-more_data_structures/6-print_sorted_dictionary.py
+++ python-more_data_structures/6-print_sorted_dictionary.py
@@ -2,4 +2,4 @@
 def print_sorted_dictionary(a_dictionary):
     sort_keys = sorted(a_dictionary)
     for i in sort_keys:
-        print("{}:{}".format(i, a_dictionary[i]))
+        print(f"{i}: {a_dictionary[i]}")
