65700aadaf627734eb3ccee8aabad20ca471ff06
Kevin Vang <kevinvaj88@hotmail.com>
2024-01-09 17:59:40 -0800

    space editing

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -1,6 +1,6 @@
 def square_matrix_simple(matrix=[]):
     new_matrix = []
     for i in matrix:
-        new_i = [elem** 2 for elem in i]
+        new_i = [elem ** 2 for elem in i]
         new_matrix.append(new_i)
     return new_matrix
