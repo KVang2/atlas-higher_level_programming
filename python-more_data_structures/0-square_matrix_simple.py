d6f5f13e8f99ff64c41d435afdd45449b191980d
Kevin Vang <kevinvaj88@hotmail.com>
2024-01-09 18:12:20 -0800

    space test

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -1,6 +1,6 @@
 def square_matrix_simple(matrix=[]):
     new_matrix = []
     for row in matrix:
-        new_row = [i ** 2 for i in row]
+        new_row = [i**2 for i in row]
         new_matrix.append(new_row)
     return new_matrix
