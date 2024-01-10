23d60f7879849ea5ddedcae261c489d63156f4f4
Kevin Vang <kevinvaj88@hotmail.com>
2024-01-09 20:43:03 -0800

    spacing

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -3,7 +3,7 @@ def safe_print_list(my_list=[], x=0):
     count = 0
     try:
         for elements in range(x):
-            print(my_list[elements], end=" ")
+            print(my_list[elements], end="")
             count += 1
     except IndexError:
         pass
