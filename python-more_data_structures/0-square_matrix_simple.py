ab60a009ed6339d61b06e28e85c408aae521ef21
Kevin Vang <kevinvaj88@hotmail.com>
2024-01-09 18:22:20 -0800

    forgot to include #!

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -1,8 +1,7 @@
+#!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
     new_matrix = []
     for row in matrix:
-        new_row = []
-        for i in row:
-            new_row.append(i ** 2)
+        new_row = [i ** 2 for i in row]
         new_matrix.append(new_row)
     return new_matrix
