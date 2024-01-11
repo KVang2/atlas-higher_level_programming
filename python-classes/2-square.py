931d5e0155b364b998d8bc61b20b67f86a0d895a
Kevin Vang <kevinvaj88@hotmail.com>
2024-01-11 10:42:16 -0800

    square size

---
+++ python-classes/2-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Size validation"""
+
+
+class Square:
+    """objective"""
+    def __init__(self, size=0):
+        self.__size = size
+        if type(size) != int:
+            raise TypeError("size must be an integer") 
+        if size < 0:
+            raise ValueError("size must be >= 0")
