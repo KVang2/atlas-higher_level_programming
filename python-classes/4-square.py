4f6e014362cdbf2f462c66201119cf5958e9fadd
Kevin Vang <kevinvaj88@hotmail.com>
2024-01-15 12:00:39 -0800

    set property

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -5,21 +5,21 @@
 class Square:
     """objective"""
     def __init__(self, size=0):
-
+        self.__size = size
         if type(size) != int:
             raise TypeError("size must be an integer")
         if size < 0:
             raise ValueError("size must be >= 0")
-        self.__size = size
 
     """area def"""
     def area(self):
         return self.__size ** 2
 
-    """ size"""
+    @property
     def size(self):
         return self.__size
 
+    @size.setter
     def size(self, value):
         if type(value) != int:
             raise TypeError("size must be an integer")
