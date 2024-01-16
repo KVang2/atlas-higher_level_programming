495b6321e7ddf6db09b3506e509c7bd590bead30
Kevin Vang <kevinvaj88@hotmail.com>
2024-01-16 14:42:34 -0800

    small changes

--- python-test_driven_development/4-print_square.py
+++ python-test_driven_development/4-print_square.py
@@ -5,7 +5,7 @@
 def print_square(size):
    """square size"""
    if not isinstance(size, int):
-            raise TypeError("size must be >= 0")
+            raise TypeError("size must be an integer")
    if size < 0:
             raise ValueError("size must be >= 0")
    if isinstance(size, float) and size < 0:
