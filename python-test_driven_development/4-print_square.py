f0b8245e6d5736b9011cddd9d8610643a7024817
Kevin Vang <kevinvaj88@hotmail.com>
2024-01-16 14:54:36 -0800

    test file

--- python-test_driven_development/4-print_square.py
+++ python-test_driven_development/4-print_square.py
@@ -10,5 +10,5 @@ def print_square(size):
             raise ValueError("size must be >= 0")
    if isinstance(size, float) and size < 0:
             raise ValueError("size must be an integer")
-   for i in range(size):
+   for _ in range(size):
             print(f"#" * size)
