b38253459ec19faf48c0ba26cbe66510afb34e92
Kevin Vang <kevinvaj88@hotmail.com>
2024-01-16 10:48:05 -0800

    testing say name file

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -3,6 +3,9 @@
 
 
 def say_my_name(first_name, last_name=""):
-    if not isinstance(first_name, str) or not instance(last_name, str):
-        raise TypeError("first_name must be a string, and last_name must be a string
+    """Printing name"""
+    if not isinstance(first_name, str):
+        raise TypeError("first_name must be a string")
+    if not isinstance(last_name, str):
+        raise TypeError("last_name must be a string")
         print(f"My name is {first_name} {last_name}")
