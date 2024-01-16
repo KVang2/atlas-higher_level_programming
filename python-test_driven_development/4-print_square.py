860dd7e4de57250942611884b5ae3fe137995665
Kevin Vang <kevinvaj88@hotmail.com>
2024-01-16 14:31:27 -0800

    test

--- python-test_driven_development/4-print_square.py
+++ python-test_driven_development/4-print_square.py
@@ -6,9 +6,9 @@ def print_square(size):
    """square size"""
    if not isinstance(size, int):
             raise TypeError("size must be >= 0")
-   if isinstance(size, float) and size < 0:
-            raise TypeError("size must be >= 0")
    if size < 0:
+            raise ValueError("size must be >= 0")
+   if isinstance(size, float) and size < 0:
             raise ValueError("size must be >= 0")
    for i in range(size):
             print(f"#" * size)
