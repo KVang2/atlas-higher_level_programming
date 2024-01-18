b442a4ce0d8b6759faf2ce021ee84081458c8284
Kevin Vang <kevinvaj88@hotmail.com>
2024-01-17 19:15:53 -0800

    file 5

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -7,6 +7,9 @@ def text_indentation(text):
     if not isinstance(text, str):
         raise TypeError("text must be a string")
 
+    if text[-1] in ['.', '?' ':']:
+        text += '\n\n'
+
     text = text.replace('.', '.\n\n').replace('?', '?\n\n').replace(':', ':\n\n')
     text = '\n'.join(line.strip() for line in text.split('\n'))
     print(text)
