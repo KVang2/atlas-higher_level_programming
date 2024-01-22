73c0800029d458b9046d4549a4b658b7aeadef38
Kevin Vang <kevinvaj88@hotmail.com>
2024-01-22 13:15:34 -0800

    only sub class

---
+++ python-inheritance/4-inherits_from.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""A Function that returns true if object is instance of class"""
+
+
+def inherits_from(obj, a_class):
+    """checking if inherited from specified class"""
+    if isinstance(obj, a_class):
+        return True
+    else:
+        return False
