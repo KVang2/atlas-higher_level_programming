76a165421e1ab1e8c54a7057ce46d136dfa800ff
Kevin Vang <kevinvaj88@hotmail.com>
2024-01-22 12:47:53 -0800

    test

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -1,1 +1,12 @@
 #!/usr/bin/python3
+""" Function that writes class that inherits from list"""
+
+
+class MyList(list):
+    """define class and printing in sorted order"""
+    def __init__(self, int_list):
+        super().__init__(int_list)
+
+    def print_sorted(self):
+        sorted_list = sorted(self)
+        print(sorted_list)
