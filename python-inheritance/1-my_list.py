3d9f1eb380782fe3ddf3314d944b4b025493f675
Kevin Vang <kevinvaj88@hotmail.com>
2024-01-22 12:49:55 -0800

    test

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -4,9 +4,6 @@
 
 class MyList(list):
     """define class and printing in sorted order"""
-    def __init__(self, int_list):
-        super().__init__(int_list)
-
     def print_sorted(self):
         sorted_list = sorted(self)
         print(sorted_list)
