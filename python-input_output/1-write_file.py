444b7467d638c97f5aee018749c12b9dfc37d2b2
Kevin Vang <kevinvaj88@hotmail.com>
2024-01-23 09:08:23 -0800

    syntax

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -2,14 +2,14 @@
 """ A function that appends string at the end of a text file"""
 
 
-if __name__ == "__main__":
-    write_file("my_first_file.txt", "School is alright!\n")
-    return write_file
-
-
 def append_write(filename="", text=""):
     """Write a string to UTF8"""
 
     lines = 0
     with open(filename, 'w', encoding='utf-8') as file:
     return file.write(text)
+
+
+if __name__ == "__main__":
+    write_file("my_first_file.txt", "School is alright!\n")
+    return write_file
