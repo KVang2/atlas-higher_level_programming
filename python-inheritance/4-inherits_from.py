e7b1f51652a5d4ec1673a57b42b0e1ba2fbf8e10
Kevin Vang <kevinvaj88@hotmail.com>
2024-01-22 22:22:57 -0800

    test files

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -4,4 +4,4 @@
 
 def inherits_from(obj, a_class):
     """checking if inherited from specified class"""
-    return isinstance(type(obj), a_class)
+    return isinstance(type(obj), a_class) and ype(obj) is not a_class
