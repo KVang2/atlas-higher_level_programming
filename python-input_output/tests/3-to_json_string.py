2e28dfa5a266bcccc92cd216e83e747924eefaf6
Kevin Vang <kevinvaj88@hotmail.com>
2024-01-23 13:15:34 -0800

    test

--- python-input_output/tests/3-to_json_string.py
+++ python-input_output/tests/3-to_json_string.py
@@ -1,25 +1,20 @@
 # Json test
 
 if __name__ == "__main__":
-
->>> my_list = [6, 12, 24]
->>> s_my_list = to_json_string(my_list)
->>> print(s_my_list)
-'[ 6, 12, 24]'
->>> print(type(s_my_list))
-'<class 'str'>"
+    my_list = [6, 12, 24]
+    s_my_list = to_json_string(my_list)
+    print(s_my_list)
+    print(type(s_my_list))
 
 # Test 2
->>> my_dict = {'id': 24, 'name': 'Nick', 'places': ["San Franciso", "Tokyo"],
+    my_dict = {'id': 24, 'name': 'Nick', 'places': ["San Franciso", "Tokyo"],
     'is_active': True,
     'info': {
         'age': 28,
-        'average': 88}}'
->>> print(type(s_my_dict))
-'<class 'str'>"
+        'average': 88}}
 
 # Test 3
->>> my_set = { 44, 22 }
+    my_set = { 44, 22 }
     s_my_set = to_json_string(my_set)
     print(s_my_set)
     print(type(s_my_set))
