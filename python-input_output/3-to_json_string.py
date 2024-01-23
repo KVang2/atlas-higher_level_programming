8179fa927dae6038922f4e176ec7c680f18d1ac2
Kevin Vang <kevinvaj88@hotmail.com>
2024-01-23 13:22:23 -0800

    file 3

--- python-input_output/3-to_json_string.py
+++ python-input_output/3-to_json_string.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 """A function that returns the JSON representation of an object"""
 
+import json
+
 
 def to_json_string(my_obj):
     """JSON representing an object(string)"""
 
-    return json.dumps(obj)
+    return json.dumps(var(obj))
