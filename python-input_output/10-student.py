ab1485d0d54c211a42f41d8bbfa24278a7146a07
Kevin Vang <kevinvaj88@hotmail.com>
2024-01-23 16:59:06 -0800

    small fix

--- python-input_output/10-student.py
+++ python-input_output/10-student.py
@@ -16,6 +16,6 @@ def to_json(self, attrs=None):
             "first_name": self.first_name,
             "last_name": self.last_name,
             "age": self.age
-            }
+        }
     else:
         return {attr: getattr(self, attr) for attr in attrs if hasattr(self, attr)}
