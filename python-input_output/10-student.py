0492906fdad5bd7701c0d8d2c499da5a0080f947
Kevin Vang <kevinvaj88@hotmail.com>
2024-01-23 16:56:03 -0800

    students to JSON with filter

---
+++ python-input_output/10-student.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Write a class student that defines a student"""
+
+
+class Student:
+    def __init__(self, first_name, last_name, age):
+        """Instance attributes"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+def to_json(self, attrs=None):
+    """dictionary representation of a student"""
+    if attrs is None:
+        return {
+            "first_name": self.first_name,
+            "last_name": self.last_name,
+            "age": self.age
+            }
+    else:
+        return {attr: getattr(self, attr) for attr in attrs if hasattr(self, attr)}
