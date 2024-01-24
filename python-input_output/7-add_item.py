55458c2dfdb28ba21c8afaf42c778fa75762af8b
Kevin Vang <kevinvaj88@hotmail.com>
2024-01-23 17:57:10 -0800

    add item

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 """A script that adds all arguments to a python list, then save"""
 import sys
-from save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-from load_from_json_file = __import__('6-load_from_json_file')
+from save_to_json_file = import ('5-save_to_json_file').save_to_json_file
+from load_from_json_file = import ('6-load_from_json_file')
 
 filename = 'add_item.json'
 my_list = []
