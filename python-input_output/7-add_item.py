e6bac8940701d14f4f3dadec10d004b55bf2ea6a
Kevin Vang <kevinvaj88@hotmail.com>
2024-01-23 18:07:59 -0800

    spacing errors

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -9,12 +9,14 @@ def save_to_json_file(my_obj, filename):
     with open(filename, 'w') as file:
         json.dump(my_obj, file)
 
+
 def load_from_json_file(filename):
     """Open load from json file"""
     with open(filename, 'r', encoding='utf-8') as file:
         return json.load(file)
 
 filename = 'add_item.json'
+
 
 try:
     my_list = load_from_json_file(filename)
