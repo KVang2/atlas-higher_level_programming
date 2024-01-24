8b6cfefb2593a54c1ae86ab0bb53633cbf26aadd
Kevin Vang <kevinvaj88@hotmail.com>
2024-01-23 18:05:39 -0800

    spacing issue

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -19,7 +19,7 @@ filename = 'add_item.json'
 try:
     my_list = load_from_json_file(filename)
 except FileNotfoundError:
-    my_list =[]
+    my_list = []
 
 for arg in sys.argv[1:]:
     my_list.append(arg)
