3170ca6dc329ac84ed29600a83a18a5eb942e228
kvang2 <kevinvaj88@hotmail.com>
2024-01-27 10:37:59 -0600

    typo

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -11,7 +11,5 @@ def __init__(self, id=None):
     if id is not None:
         self.id = id
     else:
-        base.__nb_objects += 1
-        self.id = Base.__nb_objects
-
-if __name__ == "__main__":
+        Base.__nb_objects += 1
+        self.id = Base.__nb_object:
