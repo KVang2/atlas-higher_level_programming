a8defbd84335cdd1bf3c02721c6fa25bd9021a5b
kvang2 <kevinvaj88@hotmail.com>
2024-01-26 20:52:45 -0600

    files update

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -2,3 +2,12 @@
 """Base class"""
 
 
+class Base:
+    __nb_objects = 0
+
+def __init__(self, id=None):
+    if id is not None:
+        self.id = id
+    else:
+        base.__nb_objects += 1
+        self.id = Base.__nb_objects
