c0984b3bd5b9336de2d435321a337b25716a67e8
Kevin Vang <kevinvaj88@hotmail.com>
2024-01-28 12:37:20 -0800

    fixing files

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -6,10 +6,10 @@ class Base:
     """Base class"""
     __nb_objects = 0
 
-def __init__(self, id=None):
+    def __init__(self, id=None):
     """class attribute"""
-    if id is not None:
-        self.id = id
-    else:
-        Base.__nb_objects += 1
-        self.id = Base.__nb_object
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_object
