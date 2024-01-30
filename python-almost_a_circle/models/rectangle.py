0a5d7ba43157f9788f50a15a61982be72c1b6a4b
kvang2 <kevinvaj88@hotmail.com>
2024-01-30 01:50:10 -0600

    "file update"

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -9,6 +9,7 @@ class Rectangle(Base):
     def __init__(self, width, height, x=0, y=0, id=None):
         """Attribute of width height"""
         super().__init__(id)
+        self.id = id
         self.width = width
         self.height = height
         self.x = x
