44f66a1170540d3f1a4639e8df81a2b7e20d1c54
kvang2 <kevinvaj88@hotmail.com>
2024-01-30 13:25:05 -0600

    "minor error"

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -17,6 +17,6 @@ class Base:
     def to_json_string(list_dictionaries):
         """returning JSON string representation of list_dictionaries"""
         if list_dictionaries is None or list_dictionaries == []:
-            return ""
+            return "[]"
         else:
             return json.dumps(list_dictionaries)
