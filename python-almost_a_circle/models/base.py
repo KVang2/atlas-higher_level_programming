a21e8759c542bec3d7ea17f84c6bd9fdc9e93108
kvang2 <kevinvaj88@hotmail.com>
2024-01-30 13:53:10 -0600

    file updates

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -16,7 +16,7 @@ class Base:
 
     def to_json_string(list_dictionaries):
         """returning JSON string representation of list_dictionaries"""
-        if list_dictionaries is None or list_dictionaries == []:
+        if list_dictionaries is None or not list_dictionaries:
             return "[]"
         else:
             return json.dumps(list_dictionaries)
