2a70da6d9212e6c64c81ba174d19cdf4bf9bcfbf
kvang2 <kevinvaj88@hotmail.com>
2024-01-30 11:10:49 -0600

    update attributes

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -90,13 +90,15 @@ class Rectangle(Base):
         """Upates on Rectangle"""
         if args:
             if len(args) >= 1:
-                self.id = args[0]
+                self.__id = args[0]
             if len(args) >= 2:
-                self.size = args[1]
+                self.__width = args[1]
             if len(args) >= 3:
-                self.x = args[2]
+                self.__height = args[2]
             if len(args) >= 4:
-                self.y = args[3]
+                self.__x = args[3]
+            if len(args) >= 5:
+                self.__y = args[4]
         else:
             for fields, value in kwargs.items():
                 setattr(self, fields, value)
