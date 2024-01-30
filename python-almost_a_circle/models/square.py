7bc60bf46ffef0b204eebd92fe33cec0759626b6
kvang2 <kevinvaj88@hotmail.com>
2024-01-30 10:24:50 -0600

    Update square file

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -8,17 +8,16 @@ class Square(Rectangle):
     def __init__(self, size, x=0, y=0, id=None):
         """calling super class"""
         super().__init__(size, size, x, y, id)
-    
+
     def __str__(self):
         """str def"""
-        return "[Square] ({}) {}/{} - {}".format(
-            self.id, self.x, self.y, self.width)
+        return "[Square] ({}) {}/{} - {}".format(self.id, self.x, self.y, self.width)
 
     @property
     def size(self):
         """property size"""
         return self.width
-    
+
     @size.setter
     def size(self, value):
         """setter for size"""
