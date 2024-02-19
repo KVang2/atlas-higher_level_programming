7c56b550358eaef63f4d15165a01d2dde7a42e9e
kvang2 <kevinvaj88@hotmail.com>
2024-02-19 14:06:31 -0600

    chmod

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -2,4 +2,4 @@
 -- Password set to user_0d_1-pwd
 CREATE USER if not exist 'user_0d_1'@'localhost'
 IDENTIFIED BY 'user_0d_1_pwd'
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';clear
