d78bfe31eeeed149e59c467d029fc0edd392bf88
kvang2 <kevinvaj88@hotmail.com>
2024-02-19 16:57:15 -0600

    adding both tables from 3 and 4 (force_name and id_not_null)

---
+++ SQL_more_queries/3-force_name.sql
@@ -0,0 +1,7 @@
+-- Creates table: force_name
+-- Containing id INT and name VARCHAR(256)
+CREATE TABLE IF NOT EXISTS force_name
+(
+    id INT,
+    name VARCHAR(256) NOT NULL
+);
---
+++ SQL_more_queries/4-never_empty.sql
@@ -0,0 +1,7 @@
+-- Creates table: id_not_null
+-- Table contains id INT with default value 1 and name VARCHAR(256)
+CREATE TABLE IF NOT EXISTS id_not_null
+(
+    id INT DEFAULT 1,
+    name VARCHAR(256) NOT NULL
+);
