d3abf1b667066324dc8995f719a1d3b7c8beca8d
kvang2 <kevinvaj88@hotmail.com>
2024-02-19 14:29:53 -0600

    giving user permission

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,5 +1,5 @@
 -- Creates server user having all privileges on server
 -- Password set to user_0d_1-pwd
 CREATE USER if not exist 'user_0d_1'@'localhost'
-IDENTIFIED BY 'user_0d_1_pwd'
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';clear
+IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
