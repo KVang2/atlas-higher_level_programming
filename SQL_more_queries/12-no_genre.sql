56423850716986962363f93296c2932438bedd9b
kvang2 <kevinvaj88@hotmail.com>
2024-02-20 11:40:45 -0600

    forgot to add tv show genres

--- SQL_more_queries/12-no_genre.sql
+++ SQL_more_queries/12-no_genre.sql
@@ -1,5 +1,5 @@
 -- Importing database dump from tvshow in MySQL server
-SELECT hbtn_0d_tvshows
+SELECT hbtn_0d_tvshows, tv_show_genres.genre_id
 FROM hbtn_0d_tvshows
 LEFT JOIN tv_show_genres
 ON tv_show.id = tv_show_genres.show_id
