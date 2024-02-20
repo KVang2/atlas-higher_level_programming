1519f1daeab5799cdd446baa4c2bf3e0ce21cd75
kvang2 <kevinvaj88@hotmail.com>
2024-02-19 20:12:23 -0600

    include use hbtn_0d_usa

--- SQL_more_queries/6-states.sql
+++ SQL_more_queries/6-states.sql
@@ -1,9 +1,12 @@
 -- Create database (hbtn_0d_usa) and table (states)
 -- Include id INT unique, name VARCHAR(256)
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
-CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states
+
+USE hbtn_0d_usa;
+
+CREATE TABLE IF NOT EXISTS states
 (
     id INT NOT NULL AUTO_INCREMENT UNIQUE,
-    name VACHAR(256) NOT NULL,
+    name VARCHAR(256) NOT NULL,
     PRIMARY KEY (id)
 );
