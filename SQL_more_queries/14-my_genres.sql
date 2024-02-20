603f743441b23b5bdf67c7af1554f13372e9b0b3
kvang2 <kevinvaj88@hotmail.com>
2024-02-20 12:45:48 -0600

    debug code

--- SQL_more_queries/14-my_genres.sql
+++ SQL_more_queries/14-my_genres.sql
@@ -1,5 +1,5 @@
 -- A database to lists all genres of the show
-SELECT tv_genres.name FROM tv_shows
+SELECT tv_genres.name FROM tv_show_genres
 JOIN tv_shows ON tv_shows_genres.show_id = tv_shows.id
 JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
 WHERE tv_shows.title = "Dexter" ORDER BY tv_genres.name ASC;
