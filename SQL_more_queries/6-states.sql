881cacdee857ba19101ddd3b44d9297fec8324a1
kvang2 <kevinvaj88@hotmail.com>
2024-02-19 20:05:09 -0600

    making key primary

--- SQL_more_queries/6-states.sql
+++ SQL_more_queries/6-states.sql
@@ -1,11 +1,12 @@
 -- Create database (hbtn_0d_usa) and table (states)
 -- Include id INT unique, name VARCHAR(256)
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+
 USE hbtn_0d_usa;
 
 CREATE TABLE IF NOT EXISTS states
 (
     id INT NOT NULL AUTO_INCREMENT UNIQUE,
-    name VACHAR(256) NOT NULL
+    name VACHAR(256) NOT NULL,
     PRIMARY KEY (id)
 );
