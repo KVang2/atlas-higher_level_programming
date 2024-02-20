4b0932cd8971aed6c7dbc9a37313169dabfa354d
kvang2 <kevinvaj88@hotmail.com>
2024-02-20 11:53:03 -0600

    listed all shows without a genre linked

--- SQL_more_queries/12-no_genre.sql
+++ SQL_more_queries/12-no_genre.sql
@@ -1,5 +1,5 @@
 -- Importing database dump from tvshow in MySQL server
-SELECT hbtn_0d_tvshows, tv_show_genres.genre_id
+SELECT tv_shows.title, tv_show_genres.genre_id
 FROM tv_shows
 LEFT JOIN tv_show_genres
 ON tv_show.id = tv_show_genres.show_id
