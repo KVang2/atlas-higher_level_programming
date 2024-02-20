1e523f3e4a8c044c78b5b473d79aef389d0b342b
kvang2 <kevinvaj88@hotmail.com>
2024-02-20 14:38:50 -0600

    debug task 15 for join

--- SQL_more_queries/15-comedy_only.sql
+++ SQL_more_queries/15-comedy_only.sql
@@ -1,6 +1,6 @@
 -- Lists all Comdey shows in database
 -- Table contains one recodrd where name = Comedy
 SELECT tv_shows.title FROM tv_shows
-JOIN tv_genres_shows ON tv_shows.id = tv_genres_shows.show_id
-JOIN tv_genres ON tv_genres.id = tv_genres.genre_id
-WHERE tv_shows.name = "Comedy" ORDER BY tv_genres.name ASC;
+JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+JOIN tv_genres ON tv_genres.id = tv_show_genres.genre_id
+WHERE tv_genres.name = "Comedy" ORDER BY tv_shows.title ASC;
