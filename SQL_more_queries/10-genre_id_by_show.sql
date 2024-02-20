33ad0c1c228c287019fcf669b715b7cc88a8f5ba
kvang2 <kevinvaj88@hotmail.com>
2024-02-19 21:15:16 -0600

    import database from tv shows

---
+++ SQL_more_queries/10-genre_id_by_show.sql
@@ -0,0 +1,4 @@
+-- Import database dump from hbtn_0d_tvshows
+SELECT tv_shows.title, tv_show_genres.genre_id FROM tv_shows
+ON tv_shows.id = tv_show_genres.tv_show_id
+ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
