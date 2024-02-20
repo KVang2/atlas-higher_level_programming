093455bfb74c8c32549f31c0b2bf1a248ec3b5cd
kvang2 <kevinvaj88@hotmail.com>
2024-02-20 11:50:57 -0600

    script that listed all shows without genre linked

--- SQL_more_queries/12-no_genre.sql
+++ SQL_more_queries/12-no_genre.sql
@@ -3,5 +3,5 @@ SELECT hbtn_0d_tvshows, tv_show_genres.genre_id
 FROM tv_shows
 LEFT JOIN tv_show_genres
 ON tv_show.id = tv_show_genres.show_id
-WHERE tv_show_genres.show_id IS NULL
+WHERE tv_show_genres.genre_id IS NULL
 ORDER BY tv_show.title ASC; 
