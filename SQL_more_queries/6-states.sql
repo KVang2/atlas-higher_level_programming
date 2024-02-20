e2a557ba7b0d92547cb63cc3322c660392f5bd73
kvang2 <kevinvaj88@hotmail.com>
2024-02-19 20:10:30 -0600

    fixing so table and database exist

--- SQL_more_queries/6-states.sql
+++ SQL_more_queries/6-states.sql
@@ -1,9 +1,6 @@
 -- Create database (hbtn_0d_usa) and table (states)
 -- Include id INT unique, name VARCHAR(256)
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
-
-USE hbtn_0d_usa;
-
 CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states
 (
     id INT NOT NULL AUTO_INCREMENT UNIQUE,
