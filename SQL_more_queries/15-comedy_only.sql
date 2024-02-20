8e9bb577bc1e29479a73c0a6788d16cc9df7f639
kvang2 <kevinvaj88@hotmail.com>
2024-02-20 14:05:57 -0600

    lists comedy show in database

---
+++ SQL_more_queries/15-comedy_only.sql
@@ -0,0 +1,5 @@
+-- Lists all Comdey shows in database
+-- Table contains one recodrd where name = Comedy
+SELECT tv_shows.title FROM tv_shows
+JOIN tv_genres_shows ON tv_shows.id = tv_genres_shows.show_id
+JOIN tv_genres ON tv_genres_shows.genre_id = tv_genres.id;
