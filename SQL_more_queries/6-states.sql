74f6a73b36a9c6c14e7ff037d4ec307392158537
kvang2 <kevinvaj88@hotmail.com>
2024-02-19 19:50:26 -0600

    created a database name hbtn_0d_usa and table name states task 6

---
+++ SQL_more_queries/6-states.sql
@@ -0,0 +1,10 @@
+-- Create database (hbtn_0d_usa) and table (states)
+-- Include id INT unique, name VARCHAR(256)
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+USE hbtn_0d_usa;
+
+CREATE TABLE IF NOT EXISTS states
+(
+    id INT DEFAULT 1 UNIQUE,
+    name VACHAR(256) NOT NULL
+);
