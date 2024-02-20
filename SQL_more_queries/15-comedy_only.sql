610ba3bbcd132675b4f8a51bc7dc9fede0aab058
kvang2 <kevinvaj88@hotmail.com>
2024-02-20 14:11:25 -0600

    sort comedy show listed in asc order

--- SQL_more_queries/15-comedy_only.sql
+++ SQL_more_queries/15-comedy_only.sql
@@ -2,4 +2,5 @@
 -- Table contains one recodrd where name = Comedy
 SELECT tv_shows.title FROM tv_shows
 JOIN tv_genres_shows ON tv_shows.id = tv_genres_shows.show_id
-JOIN tv_genres ON tv_genres_shows.genre_id = tv_genres.id;
+JOIN tv_genres ON tv_genres.id = tv_genres.genre_id
+WHERE tv_shows.name = "Comedy" ORDER BY tv_genres.name ASC;
