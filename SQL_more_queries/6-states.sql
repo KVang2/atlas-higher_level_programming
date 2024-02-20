cb86e8b5aa7f33c1aa30260fb3a615b780cc356d
kvang2 <kevinvaj88@hotmail.com>
2024-02-19 19:56:18 -0600

    add auto increment for auto generated and is primary key

--- SQL_more_queries/6-states.sql
+++ SQL_more_queries/6-states.sql
@@ -1,10 +1,9 @@
 -- Create database (hbtn_0d_usa) and table (states)
 -- Include id INT unique, name VARCHAR(256)
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
-USE hbtn_0d_usa;
-
 CREATE TABLE IF NOT EXISTS states
 (
-    id INT DEFAULT 1 UNIQUE,
+    id INT NOT NULL AUTO_INCREMENT UNIQUE,
     name VACHAR(256) NOT NULL
+    PRIMARY KEY (id)
 );
