22915bb9993376a949e072ef5a34867be9142e0b
kvang2 <kevinvaj88@hotmail.com>
2024-02-20 21:19:52 -0600

    add comments

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -7,18 +7,23 @@ import sys
 
 def mysql_connect():
     """Function that connects to the MySQL server"""
+    #Retrieve connection details from cmd-line arg.
     username = sys.argv[1]
     password = sys.argv[2]
     db_name = sys.argv[3]
 
-    db = MySQLdb.connect(host="localhost",
-                        user=username,
-                        port=3306,
-                        passwd=password,
-                        database=db_name)
+    # Connect MySQL server
+    db = MySQLdb.connect(
+        host="localhost",
+        user=username,
+        port=3306,
+        passwd=password,
+        database=db_name)
 
+    # Create cursor object to interact with database
     mycursor = db.cursor()
 
+    # Execute SQL query to select all startes from table
     mycursor.execute("SELECT * FROM states ORDER BY id ASC")
 
     rows = mycursor.fetchall()
@@ -26,9 +31,12 @@ def mysql_connect():
     for row in rows:
         print(row)
 
+    # Close cursor and database connection
     mycursor.close()
     db.close()
 
 
+# Check if script is executed directly
 if __name__ == "__main__":
+    # Call mysql_connect function to execute script
     mysql_connect()
