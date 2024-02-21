1c9d7f648cef83b11a3553b05984982dc6125845
Kevin Vang <kevinvaj88@hotmail.com>
2024-02-20 17:51:43 -0800

    function that takes 3 argument and list all states from database

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -5,4 +5,26 @@ import sys
 import MySQLdb
 
 
-def main():
+def mysql_connect(usr, pw, db_name):
+    """Function that connects to the MySQL server"""
+    db = MySQLdb.connect(host="localhost",
+            user=usr,
+            port=3306,
+            passwd=pw,
+            database=db_name)
+
+    mycursor = mydb.cursor()
+
+    mycursor.execute("SELECT * FROM states ORDER BY id")
+
+    rows = cursor.fetchall()
+
+    for row in rows;
+        print(row)
+
+    mycursor.close()
+    db.close()
+
+
+if __name__ == "__main__":
+    main()
