d8304ce0d14d7c4304c5244b1930443b319c0bd2
kvang2 <kevinvaj88@hotmail.com>
2024-03-25 09:23:39 -0500

    concat two string

---
+++ javascript-warm_up/4-concat.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+
+// Checking if tow arguments are provided
+if (process.argv[2] !== undefined && process.argv[3] !== undefined) {
+    console.log(process.argv[2] + " is " + process.argv[3]);
+} else {
+    console.log('Please provide at least two arguments.')
+}
