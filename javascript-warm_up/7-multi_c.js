3894a6a481cc660c9ddbd89bbfa2e7b71cae3abd
kvang2 <kevinvaj88@hotmail.com>
2024-03-26 00:03:48 -0500

    chmod

--- javascript-warm_up/7-multi_c.js
+++ javascript-warm_up/7-multi_c.js
@@ -5,9 +5,9 @@ const num = parseInt(process.argv[2]);
 
 // Check if argument can be converted to integer
 if (!isNaN(num)) {
-    for (let i = 0; i < num; i++) {
-        console.log('C is fun');
+  for (let i = 0; i < num; i++) {
+    console.log('C is fun');
   }
 } else {
-    console.log('Missing number of occurrences', num);
+  console.log('Missing number of occurrences', num);
 }
