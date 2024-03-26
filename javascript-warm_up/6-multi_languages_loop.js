e9800519ac8fb845126f6978b32f4bf31506dd73
kvang2 <kevinvaj88@hotmail.com>
2024-03-25 19:38:58 -0500

    setting for loop

---
+++ javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+
+// Setting array of string
+const array = [
+  'C is fun',
+  'Python is cool',
+  'JavaScript is amazing'
+];
+
+// Looping through array and printing out string
+for (let i = 0; i < array.length; i++) {
+  console.log(array[1]);
+}
