a0bda4daa09919e203810e5345c7ce834dd8be34
kvang2 <kevinvaj88@hotmail.com>
2024-03-26 09:55:19 -0500

    addition of 2 int

--- javascript-warm_up/9-add.js
+++ javascript-warm_up/9-add.js
@@ -3,6 +3,8 @@
 function add(a, b) {
   if (!isNaN(a) && !isNaN(b)) {
     return parseInt(a) + parseInt(b);
+  } else {
+    return NaN;
   }
 }
 
@@ -10,8 +12,5 @@ const arg1 = process.argv[2];
 const arg2 = process.argv[3];
 
 const result = add(arg1, arg2);
-if (result !== undefined) {
     console.log(result);
-} else {
-    console.log(arg1 + arg2);
-}
+
