e8032758024d8810be6afe99e41a17f7eaed6e21
kvang2 <kevinvaj88@hotmail.com>
2024-03-26 22:33:24 -0500

    reverse list

---
+++ javascript_objects_scopes_closures/8-esrever.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+exports.esrever = function (list) {
+    // Get length of list
+  const l = list.length
+
+    // Iterate over first half of the list
+    // Swap elements to reverse list
+    // Math.floor, used to convert floating point num to int
+  for (let i = 0; i < Math.floor(l / 2); i++) {
+    // swap elements using temp variable
+    const temp = list[i];
+    list[i] = list[l - 1 - i];
+    list[l - 1 - i] = temp;
+  }
+  return list;
+}
