44bc8886fab5db365d12bfa3ccdc004bdb005be1
kvang2 <kevinvaj88@hotmail.com>
2024-03-26 21:12:02 -0500

    removing this.size

--- javascript_objects_scopes_closures/5-square.js
+++ javascript_objects_scopes_closures/5-square.js
@@ -2,9 +2,8 @@
 const Rectangle = require('./4-rectangle.js');
 
 class Square extends Rectangle {
-    constructor (size) {
-        super(size, size);
-        this.size = size;
-    }
+  constructor (size) {
+    super(size, size);
+  }
 }
 module.exports = Square;
