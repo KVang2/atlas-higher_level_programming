d23b2f7ecc0d1a69394750f14228cb555c192bcf
kvang2 <kevinvaj88@hotmail.com>
2024-03-26 22:42:28 -0500

    fix semistandard validation

--- javascript_objects_scopes_closures/8-esrever.js
+++ javascript_objects_scopes_closures/8-esrever.js
@@ -1,11 +1,11 @@
 #!/usr/bin/node
 exports.esrever = function (list) {
-    // Get length of list
-  const l = list.length
+  // Get length of list
+  const l = list.length;
 
-    // Iterate over first half of the list
-    // Swap elements to reverse list
-    // Math.floor, used to convert floating point num to int
+  // Iterate over first half of the list
+  // Swap elements to reverse list
+  // Math.floor, used to convert floating point num to int
   for (let i = 0; i < Math.floor(l / 2); i++) {
     // swap elements using temp variable
     const temp = list[i];
