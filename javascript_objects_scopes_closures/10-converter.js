a2601b61a0c32e704271e9cf6a17797a7af73ff3
kvang2 <kevinvaj88@hotmail.com>
2024-03-26 23:20:25 -0500

    number conversion

--- javascript_objects_scopes_closures/10-converter.js
+++ javascript_objects_scopes_closures/10-converter.js
@@ -1,15 +1,6 @@
 #!/usr/bin/node
 exports.converter = function (base) {
-   if (base < 2 || base > 36) {
-    return 'Invalid base' ;
-   }
-   result = '';
-   remainder = 0;
-
-   while (num > 0) {
-    remainder = num % base;
-    result = remainder.toString(base) + result;
-    num = Math.floor(num / base);
-   }
-   return result || '0';
+ return function (num) {
+    return num.toString(base);
+  }
 }
