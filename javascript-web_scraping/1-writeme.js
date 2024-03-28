954c96177cc4089ea4253b16f109123ba63e7a72
kvang2 <kevinvaj88@hotmail.com>
2024-03-28 10:42:42 -0500

    define data

--- javascript-web_scraping/1-writeme.js
+++ javascript-web_scraping/1-writeme.js
@@ -2,7 +2,7 @@
 const fs = require('fs')
 const filePath = process.argv[2];
 
-fs.writeFile(filePath, 'utf-8', data, (err) => {
+fs.writeFile(filePath, 'utf-8', (err, data) => {
   if (err) {
     console.error(err)
     return;
