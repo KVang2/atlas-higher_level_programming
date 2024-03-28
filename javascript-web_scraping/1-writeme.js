6b76a95c8f9922037070a9f9a02e793eadd765e2
kvang2 <kevinvaj88@hotmail.com>
2024-03-28 10:45:17 -0500

    set data to string

--- javascript-web_scraping/1-writeme.js
+++ javascript-web_scraping/1-writeme.js
@@ -1,8 +1,9 @@
 #!/usr/bin/node
 const fs = require('fs')
 const filePath = process.argv[2];
+let data = data.toString();
 
-fs.writeFile(filePath, 'utf-8', (err, data) => {
+fs.writeFile(filePath, 'utf-8', data, (err) => {
   if (err) {
     console.error(err)
     return;
