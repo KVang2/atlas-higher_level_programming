4e601e2cd1b731b902828b58fb916c0498c8700d
kvang2 <kevinvaj88@hotmail.com>
2024-03-28 10:29:05 -0500

    semistandard validation

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -1,5 +1,8 @@
 #!/usr/bin/node
+
+// Import 'fs' module
 const fs = require('fs');
+// Get file path provided as first argument
 const filePath = process.argv[2];
 
 fs.readFile(filePath, 'utf-8', (err, data) => {
