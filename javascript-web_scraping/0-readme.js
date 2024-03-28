1bfdab34dd246cf20d8ecf05eba4bc6b6573aa26
kvang2 <kevinvaj88@hotmail.com>
2024-03-28 10:15:21 -0500

    fix script

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -1,16 +1,8 @@
 #!/usr/bin/node
 const fs = require('fs');
-const file = process.argv[2];
 
-if (!file) {
-    console.error('Error: object error')
-    process.exit(1);
-}
+fs.readFile('utf-8', (err, data) => {
+  if (err) throw err;
 
-try {
-    const fileContent = fs.readFileSync(file, 'utf-8');
-    console.log('FIle contents:', fileContent);
-} catch (error) {
-    console.error('Error reading file:', error.message);
-    process.exit(1);
-}
+  console.log(data.toString());
+})
