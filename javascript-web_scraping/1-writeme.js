ff209743ece7f9735c837e9bd1cf14424bd0249f
kvang2 <kevinvaj88@hotmail.com>
2024-03-28 20:00:32 -0500

    fixing semistandard

--- javascript-web_scraping/1-writeme.js
+++ javascript-web_scraping/1-writeme.js
@@ -1,16 +1,15 @@
 #!/usr/bin/node
-const fs = require('fs')
+const fs = require('fs');
 const filePath = process.argv[2];
 const stringWrite = process.argv[3];
 
 if (!filePath || !stringWrite) {
-  console.error('Error: file path or string not provided')
+  console.error('Error: file path or string not provided');
   process.exit(1);
 }
 
 fs.writeFile(filePath, stringWrite,  'utf-8', (err) => {
   if (err) {
-    console.error(err)
-    return;
+    console.error(err);
   }
 });
