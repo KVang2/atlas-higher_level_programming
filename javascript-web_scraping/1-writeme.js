027fb880381e0466c91269d45707bf93628fd4b1
kvang2 <kevinvaj88@hotmail.com>
2024-03-28 19:18:33 -0500

    A script that writes string to file

--- javascript-web_scraping/1-writeme.js
+++ javascript-web_scraping/1-writeme.js
@@ -1,12 +1,17 @@
 #!/usr/bin/node
 const fs = require('fs')
 const filePath = process.argv[2];
-let data = data.toString();
+const stringWrite = process.argv[3];
 
-fs.writeFile(filePath, 'utf-8', data, (err) => {
+if (!filePath || !stringWrite) {
+  console.error('Error: file path or string not provided')
+  process.exit(1);
+}
+
+fs.writeFile(filePath, stringWrite,  'utf-8', (err) => {
   if (err) {
     console.error(err)
     return;
   }
-  console.log(data);
-})
+  console.log(filePath);
+});
