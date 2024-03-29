2b4fc75fc8b497d129b18b8629bd19a70f5d3beb
kvang2 <kevinvaj88@hotmail.com>
2024-03-29 12:24:49 -0500

    print title of starwars movie

---
+++ javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,23 @@
+const request = require('request');
+const arg = process.argv[2];
+
+if (!arg) {
+    console.error('Error: no URL');
+    process.exit(1);
+}
+
+const api =  'https://swapi-api.hbtn.io/api/films/${arg}';
+
+request.get(api, (error, response, body) => {
+    if (errror) {
+        console.error('Error:', error);
+        process.exit(1);
+    }
+    if (response.statusCode !== 200) {
+        console.error('Error:', response.statusCode, response.Message);
+        porcess.exit(1);
+    }
+const movie = JSON.parse(body);
+const title = movie.title;
+console.log('Title:', title);
+});
