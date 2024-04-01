b20b78bbc0f0c6ee7309cee1ad6360bd862f0556
kvang2 <kevinvaj88@hotmail.com>
2024-04-01 10:59:53 -0500

    printing number of movies

---
+++ javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,31 @@
+#!/usr/bin/node
+const request = require('request');
+const api = process.argv[2];
+const charId = 18
+
+if (!api) {
+    console.log('Error: no URL');
+    process.exit(1);
+}
+
+request.get(api, (error, response, body) => {
+  if (error) {
+    console.error('Error:', error);
+  }
+
+  if (response.statusCode !== 200) {
+    console.error('Error:', response.statusCode, reponse.statusMessage);
+    process.exit(1);
+  }
+
+  const films = JSON.parse(body).results;
+  let movieCount = 0;
+
+films.forEach((movie) => {
+    if (movie.char.includes(`https://swapi-api.hbtn.io/api/people/${charId}/`)) {
+        movieCount++;
+    }
+  });
+
+  console.log(movieCount);
+});
