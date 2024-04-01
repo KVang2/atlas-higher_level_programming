14f2e4d3f964a4ec2567b6d14e3f0c2d82950b1f
kvang2 <kevinvaj88@hotmail.com>
2024-03-31 23:00:02 -0500

    fixing semistandard validation

--- javascript-web_scraping/3-starwars_title.js
+++ javascript-web_scraping/3-starwars_title.js
@@ -3,25 +3,25 @@ const request = require('request');
 const arg = process.argv[2];
 
 if (!arg) {
-    console.error('Error: no URL');
-    process.exit(1);
+  console.error('Error: no URL');
+  process.exit(1);
 }
-
 const api =  `https://swapi-api.hbtn.io/api/films/${arg}`;
 
 request.get(api, (error, response, body) => {
-    if (error) {
-        console.error('Error:', error);
-        process.exit(1);
-    }
-    if (response.statusCode !== 200) {
-        console.error('Error:', response.statusCode, response.statusMessage);
-        process.exit(1);
-    }
-    if (response.statusCode === 404) {
-        console.error('Error: not found');
-        process.exit(1);
-    }
+  if (error) {
+    console.error('Error:', error);
+    process.exit(1);
+  }
+
+  if (response.statusCode !== 200) {
+    console.error('Error:', response.statusCode, response.statusMessage);
+    process.exit(1);
+  }
+  if (response.statusCode === 404) {
+    console.error('Error: not found');
+    process.exit(1);
+  }
 const movie = JSON.parse(body);
 const title = movie.title;
 console.log(title);
