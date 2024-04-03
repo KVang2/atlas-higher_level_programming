5aa7cfb02df62e600e5fad697cb05fd9a3072b35
kvang2 <kevinvaj88@hotmail.com>
2024-04-02 23:01:13 -0500

    semistandard validation

--- javascript-web_scraping/5-request_store.js
+++ javascript-web_scraping/5-request_store.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 const fs = require('fs');
-const request = require ('request');
+const request = require('request');
 const url = process.argv[2];
 const filePath = process.argv[3];
 
