de33a0ae76485a6ca379fe675e6cc00dd1a2625d
kvang2 <kevinvaj88@hotmail.com>
2024-04-02 23:28:37 -0500

    semistandard vali

--- javascript-web_scraping/6-completed_tasks.js
+++ javascript-web_scraping/6-completed_tasks.js
@@ -3,32 +3,33 @@ const url = process.argv[2];
 const request = require('request');
 
 if (!url) {
-    console.log('Error: no URL');
-    process.exit(1);
+  console.log('Error: no URL');
+  process.exit(1);
 }
 
 request.get(url, (error, response, body) => {
-    if (error) {
-        console.error('Error:', error);
-        process.exit(1);
-    }
+  if (error) {
+    console.error('Error:', error);
+    process.exit(1);
+  }
 
-    if (response.statusCode !== 200) {
-        console.error('Error:', response.statusCode, response.statusMessage);
-        process.exit(1);
-    }
+  if (response.statusCode !== 200) {
+    console.error('Error:', response.statusCode, response.statusMessage);
+    process.exit(1);
+  }
 
-    const todos = JSON.parse(body);
-    const completedTasks = {};
+  const todos = JSON.parse(body);
+  const completedTasks = {};
 
-    todos.forEach((task) => {
-        if (task.completed) {
-            if (completedTasks[task.userId]) {
-                completedTasks[task.userId]++;
-            } else {
-                completedTasks[task.userId] = 1;
-            }
+  todos.forEach((task) => {
+    if (task.completed) {
+      if (completedTasks[task.userId]) {
+        completedTasks[task.userId]++;
+      } else {
+        completedTasks[task.userId] = 1;
         }
-    });
-    console.log(completedTasks);
+      }
+  });
+
+  console.log(completedTasks);
 });
