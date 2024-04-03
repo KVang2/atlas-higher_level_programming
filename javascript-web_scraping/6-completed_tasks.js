ace92b1458032d297449d291a8911a0cc8d4d3bb
kvang2 <kevinvaj88@hotmail.com>
2024-04-02 23:22:35 -0500

    computes number of tasks

--- javascript-web_scraping/6-completed_tasks.js
+++ javascript-web_scraping/6-completed_tasks.js
@@ -1,8 +1,35 @@
 #!/usr/bin/node
 const url = process.argv[2];
-const request = request ('require');
+const request = require('request');
 
 if (!url) {
     console.log('Error: no URL');
     process.exit(1);
 }
+
+request.get(url, (error, response, body) => {
+    if (error) {
+        console.error('Error:', error);
+        process.exit(1);
+    }
+
+    if (response.statusCode !== 200) {
+        console.error('Error:', response.statusCode, response.statusMessage);
+        process.exit(1);
+    }
+
+    const todos = JSON.parse(body);
+    const completeTasks = {};
+
+    todos.forEach((task) => {
+        if (task.completed) {
+            if (completedTasks[task.userId]) {
+                completedTasks[task.userId]++;
+            } else {
+                completedTasks[task.userId] = 1;
+            }
+        }
+      }
+    });
+    console.log(completedTasks);
+});
